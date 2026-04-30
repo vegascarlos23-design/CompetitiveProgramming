89fbdd992286ced152cea82b005ada1d08f96b7c
SrCarter <vegascarlos23@gmail.com>
2026-04-30 12:44:31 -0500

    Resolucion con backtracking el permutationEnumeration

---
+++ Aizu/permutationEnumeration.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long inf = 1e18;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<pair<ll,ll>>> vvp;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<long long, long long> pll;
+typedef queue<int> qi;
+typedef queue<pii> qpii;
+
+#define sz(x) ll(x.size())
+#define fore(i,a,b) for(ll i = a; i<b; i++)
+#define forr(x,v) for(auto& x : v)
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+#define cinv(vec,n) for(int i = 0; i<n; i++) cin>>vec[i];
+#define coutv(vec,n) for(int i = 0; i<n; i++) cout<<vec[i]<<' ';
+#define cin2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
+#define cout2d(vec, n, m) for(int i = 0; i < n; i++, cout << endl) for(int j = 0; j < m && cout << vec[i][j]<<' '; j++);
+
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define F first
+#define S second
+#define pb push_back
+
+#define fixed(n) fixed << setprecision(n)
+
+int n;
+vi v,used;
+
+void backtracking(int pos) {
+    if (pos == n) {
+        fore(i,0,n) cout<<v[i]<<" \n"[i+1==n];
+        return;
+    }
+
+    fore(i,1,n+1) {
+        if (!used[i]) {
+            used[i] = 1;
+            v[pos] = i;
+            backtracking(pos+1);
+            used[i] = 0;
+        }
+    }
+}
+
+void solve(){
+    cin>>n;
+    v.assign(n,0); used.assign(n+1,0);
+    backtracking(0);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    //cin >> t;
+    while(t--) solve();
+    return 0;
+}
