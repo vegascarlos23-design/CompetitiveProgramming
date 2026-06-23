e1cb0ceb7d2c943edc430e91e67e010619fb8fa7
SrCarter <vegascarlos23@gmail.com>
2026-06-23 02:01:14 -0500

    ThemeCp contest level 11 22/06/26

---
+++ ThemeCpc/Level11/23/06/26/B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long inf = 1e18;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<pair<ll,ll>>> vvp;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<bool>> vvb;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<long long, long long> pll;
+typedef queue<int> qi;
+typedef queue<pii> qpii;
+
+#define sz(x) ll(x.size())
+#define fore(i,a,b) for(ll i = a; i<b; i++)
+#define forr(x,v) for(auto& x : v)
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+#define cinv(vec,n) for(int i = 0; i<n; i++) cin>>vec[i];
+#define coutv(vec,n) for(int i = 0; i<n; i++) cout<<vec[i]<<' ';
+#define cin2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
+#define cout2d(vec, n, m) for(int i = 0; i < n; i++, cout << endl) for(int j = 0; j < m && cout << vec[i][j]<<' '; j++);
+
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define F first
+#define S second
+#define pb push_back
+
+#define fixed(n) fixed << setprecision(n)
+
+void solve(){
+    int n; cin>>n;
+
+    for(int i = 1; i<= n; i+=2) {
+        int x = i;
+        while(x <= n) {
+            cout<<x<<" ";
+            x *= 2;
+        }
+    }
+    cout<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
